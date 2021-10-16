62b5defd7188b6279ff32e47119f1066fcc10779
hhoshi1123 <hhoshi@g.ecc.u-tokyo.ac.jp>
2021-10-16 07:49:30 +0000

    メッセージ2

--- script.js
+++ script.js
@@ -4,10 +4,13 @@ function omikuji() {
   let r = Math.random();
   if (r < 0.2) {
     result.textContent = "大吉";
+    result.style.color = "red";
   } else if (r < 0.7) {
     result.textContent = "吉";
+    result.style.color = "black";
   } else {
     result.textContent = "凶";
+    result.style.color = "blue";
   }
 }
 omikujiButton.onclick = omikuji;
